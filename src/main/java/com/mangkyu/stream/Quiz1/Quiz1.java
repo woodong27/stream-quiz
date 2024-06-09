9f358dd2049fd34f7319aee5b646e880203a9197
최동우 <fdf5548@naver.com>
2024-06-09 18:49:01 +0900

    Quiz1 - quiz1, quiz3

--- src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
+++ src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -3,28 +3,35 @@ package com.mangkyu.stream.Quiz1;
 import com.opencsv.CSVReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Quiz1 {
 
     // 1.1 각 취미를 선호하는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz1() throws IOException {
-        List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+        return readCsvLines().stream()
+                .map(line -> line[1].replaceAll("\\s", ""))
+                .flatMap(hobbies -> Arrays.stream(hobbies.split(":")))
+                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> newValue += oldValue));
     }
 
     // 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz2() throws IOException {
-        List<String[]> csvLines = readCsvLines();
         return new HashMap<>();
     }
 
     // 1.3 소개 내용에 '좋아'가 몇번 등장하는지 계산하여라.
     public int quiz3() throws IOException {
-        List<String[]> csvLines = readCsvLines();
-        return 0;
+        return (int) (readCsvLines().stream()
+                .map(line -> line[2].replaceAll("\\s", ""))
+                .filter(str -> str.contains("좋아"))
+                .flatMap(str -> Stream.of(str.split("좋아", -1)))
+                .count());
     }
 
     private List<String[]> readCsvLines() throws IOException {
